583097e798721f78c08043b2eb834f0b361c04d1
tuhlopuz1 <144489830+tuhlopuz1@users.noreply.github.com>
2025-07-12 17:14:57 +0300

    deleted app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 import About from "./pages/About"
 import "./index.css";
 import WelcomePage from "./pages/Welcome";
