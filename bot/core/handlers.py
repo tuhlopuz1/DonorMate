0962d865f3e2e9edd3fb082060d5d1a030208ba0
tuhlopuz1 <tuhlopuz1@gmail.com>
2025-07-12 22:28:44 +0300

    поменял клавиатуры

--- bot/core/handlers.py
+++ bot/core/handlers.py
@@ -1,13 +1,13 @@
 from aiogram import types, Router
 from aiogram.filters import Command
-from core.keyboards import miniapp_keyboard
+from core.keyboards import inline_miniapp_keyboard
 
 router = Router()
 
 
 @router.message(Command("start"))
 async def handle_start(message: types.Message):
-    await message.answer("To open miniapp click on the button below", reply_markup=miniapp_keyboard)
+    await message.answer("To open miniapp click on the button below", reply_markup=inline_miniapp_keyboard, )
 
 
 @router.message()
