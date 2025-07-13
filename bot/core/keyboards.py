77f6562c1800266ababd1c0f7e7a5150423ae8c3
asdfrewqha <asdfrewqha@gmail.com>
2025-07-13 09:53:02 +0300

    Update keyboards.py

--- bot/core/keyboards.py
+++ bot/core/keyboards.py
@@ -1,17 +1,6 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
 from core.config import FRONTEND_URL
 
-miniapp_keyboard = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(
-                text="Открыть MiniApp",
-                web_app=WebAppInfo(url=FRONTEND_URL)
-            )
-        ]
-    ],
-    resize_keyboard=True,
-)
 
 inline_miniapp_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
