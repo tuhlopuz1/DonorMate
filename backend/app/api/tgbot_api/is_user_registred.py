0615b603111d10486db6c229969d8f52d7706e49
asdfrewqha <asdfrewqha@gmail.com>
2025-07-13 18:47:27 +0300

    fix

--- backend/app/api/tgbot_api/is_user_registred.py
+++ backend/app/api/tgbot_api/is_user_registred.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from app.models.db_adapter import adapter
-from app.models.db_tables import User, Information
+from app.models.db_tables import Information
 from app.dependencies.responses import okresponse
 
 router = APIRouter()
@@ -8,7 +8,6 @@ router = APIRouter()
 
 @router.get("/is-registred/{id}")
 async def is_user_registred(id: int):
-    user = await adapter.get_by_id(User, id)
     info = await adapter.get_by_id(Information, id)
     if info:
         return okresponse(code=200)
