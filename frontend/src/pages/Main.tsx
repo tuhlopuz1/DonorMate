48bee9b8a206ce0358e1977dae49ed748f8485af
DukeOfTimes <107131066+DukeTime@users.noreply.github.com>
2025-07-18 19:30:26 +0300

    реальное кол-во донаций и имя в мейне

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState} from "react";
 import BottomNavBar from "../components/layouts/NavBar";
 import MainTopBar from "../components/layouts/MainTopBar";
 import { FiFileText, FiCalendar } from "react-icons/fi";
@@ -5,32 +6,79 @@ import { FiFileText, FiCalendar } from "react-icons/fi";
 // import type { MedotvodData } from '../components/layouts/Medotvod';
 import EventCard from "../components/layouts/EventCard";
 import { MessageCircleQuestion, Info } from "lucide-react";
-const MainPage = () => {
+import apiRequest from "../components/utils/apiRequest";
 
+interface UserProfile {
+  user_id: number;
+  username: string;
+  tg_name: string;
+  role: string;
+  created_at: string;
+  fullname: string;
+  surname: string;
+  patronymic: string;
+  birth_date: string;
+  gender: "MALE" | "FEMALE";
+  university: string;
+  group: string;
+  weight: number;
+  chronic_disease: boolean;
+  medical_exemption: boolean;
+  donor_earlier: "YES" | "NO";
+}
+
+const MainPage = () => {
+    const [user, setUser] = useState<UserProfile | null>(null);
+    const [loadingUser, setLoadingUser] = useState(true);
     // const handleMedotvodSubmit = (data: MedotvodData) => {
     //     console.log("Медотвод получен:", data);
 
     //   };
     
     //   const openMedotvodModal = useMedotvodModal(handleMedotvodSubmit);
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+        try {
+            const res = await apiRequest({
+            url: "https://api.donor.vickz.ru/api/profile",
+            method: "GET",
+            auth: true,
+            });
 
-
+            if (!res.ok) throw new Error("Ошибка при получении профиля");
+            const data: UserProfile = await res.json();
+            setUser(data);
+        } catch (error) {
+            console.error("Ошибка загрузки профиля:", error);
+        } finally {
+            setLoadingUser(false);
+        }
+        }
+        fetchUserProfile();
+    }
+    );
 
 
 
     return (
         <div className="pt-14 pb-14 flex flex-col gap-6">
             <MainTopBar />
-
-            <div className="h-auto bg-blue-600 p-6">
-                <p className="text-2xl text-white font-sans font-bold">Привет, Никитин Владимир!</p>
-                <div className="flex flex-col items-center bg-blue-400/50 rounded-xl my-6 pt-8">
-                    <h1 className="text-4xl font-sans font-bold text-white">7</h1>
-                    <p className="text-white/75 pb-8">донаций</p>
-                </div>
+            <div>
+            {loadingUser ? (
+                <div className="h-auto bg-blue-600 p-6">
+                    <p className="text-2xl text-white font-sans font-bold"> {"Привет, " + user?.fullname + "!"}
+                </p>
+                    <div className="flex flex-col items-center bg-blue-400/50 rounded-xl my-6 pt-8">
+                        <h1 className="text-4xl font-sans font-bold text-white">7</h1>
+                        <p className="text-white/75 pb-8">донаций</p>
+                    </div>
+                </div>) : 
+                (<p className="text-2xl text-white font-sans font-bold"> Загрузка...
+                </p>)
+            }
             </div>
 
-            <div>
+                <div>
                    <p className="text-2xl text-black px-5 font-sans font-bold">Быстрые действия</p>
                 <div onClick={() => {window.location.href = '/#/add-medotvod'}} className="flex items-center justify-between px-6 bg-orange-400 h-20 m-4 rounded-lg">
                     <p className="text-white">Добавить медицинский отвод</p>
