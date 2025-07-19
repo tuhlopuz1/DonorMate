88f4e0b2a95fc95f64979cbb1e84cde0eb6b41b8
asdfrewqha <asdfrewqha@gmail.com>
2025-07-19 10:42:00 +0300

    fix

--- backend/app/api/organizers/delete_question.py
+++ backend/app/api/organizers/delete_question.py
@@ -1,13 +1,15 @@
 from typing import Annotated
 from uuid import UUID
+
 from app.dependencies.checks import check_user_token
 from app.dependencies.responses import badresponse, okresponse
 from app.models.db_adapter import adapter
-from app.models.db_tables import Questions, User
-from app.models.schemas import ProfileResponse, Role
+from app.models.db_tables import Question, User
+from app.models.schemas import Role
 from fastapi import APIRouter, Depends
 
 router = APIRouter()
+
 
 @router.delete("/delete-question/{question_id}")
 async def delete_question(
@@ -18,5 +20,5 @@ async def delete_question(
         return badresponse("Unauthorized", 401)
     if user.role != Role.ADMIN:
         return badresponse("Forbidden", 403)
-    await adapter.delete(Questions, question_id)
+    await adapter.delete(Question, question_id)
     return okresponse("Question deleted")
--- backend/app/api/organizers/get_all_questions.py
+++ backend/app/api/organizers/get_all_questions.py
@@ -3,11 +3,12 @@ from typing import Annotated
 from app.dependencies.checks import check_user_token
 from app.dependencies.responses import badresponse
 from app.models.db_adapter import adapter
-from app.models.db_tables import Questions
+from app.models.db_tables import Question
 from app.models.schemas import ProfileResponse, Role
 from fastapi import APIRouter, Depends
 
 router = APIRouter()
+
 
 @router.get("/get-all-questions")
 async def get_all_questions(user: Annotated[ProfileResponse, Depends(check_user_token)]):
@@ -15,5 +16,5 @@ async def get_all_questions(user: Annotated[ProfileResponse, Depends(check_user_
         return badresponse("Unauthorized", 401)
     if user.role != Role.ADMIN:
         return badresponse("Forbidden", 403)
-    quetions = await adapter.get_all(Questions)
+    quetions = await adapter.get_all(Question)
     return quetions
